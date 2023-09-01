b876b5c5a0827a930d669b0cfdc5b7f58313ee7b
Thalie DELACOUR <delacour.a1109@ik.me>
2023-09-02 01:17:12 +0200

    patch02

--- src/DataFixtures/PageFixtures.php
+++ src/DataFixtures/PageFixtures.php
@@ -14,20 +14,14 @@ class PageFixtures extends Fixture
     {
         $accueil = new Page();
         $accueil->setNom("Accueil");
-<<<<<<< HEAD
         $accueil->setNumPage(1);
         $accueil->setCorp("");
-=======
->>>>>>> e8f1096bfd875e55b04691171eaee5de19d085b7
         $manager->persist($accueil);
 
         $preparation = new Page();
         $preparation->setNom("Preparation au Marriages");
-<<<<<<< HEAD
         $preparation->setNumPage(2);
         $preparation->setCorp("");
-=======
->>>>>>> e8f1096bfd875e55b04691171eaee5de19d085b7
         $manager->persist($preparation);
 
         $manager->flush();
