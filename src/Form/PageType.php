91300cb4e881d9f871e3b97585ab5054d84865e0
Thalie DELACOUR <delacour.a1109@ik.me>
2023-12-16 15:51:01 +0100

    23

--- src/Form/PageType.php
+++ src/Form/PageType.php
@@ -29,7 +29,7 @@ class PageType extends AbstractType
     
                 // make it optional so you don't have to re-upload the PDF file
                 // every time you edit the Product details
-                'required' => true,
+                'required' => false,
     
                 // unmapped fields can't define their validation using annotations
                 // in the associated entity, so you can use the PHP constraint classes
